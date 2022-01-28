ca27f9b8a31098fd27c02948d3091a99abf88691
Mujahedyousef <mujahedyousef97@gmail.com>
2022-01-28 19:28:10 +0200

    update and add prototype

--- js/app.js
+++ js/app.js
@@ -1,31 +1,51 @@
 "use strict"
-let allEmployee = [];
-function InformationsEmployee(employeeID, fullName, department, level, salary){
+let taxSalary=0;
+  let salaryAfterTax=0;
+let Department=['Administration','Marketing','Development','Finance'];
+let allEmployee=[];
+function InformationsEmployee(employeeID, fullName, department, level ){
 
     this.EmployeeID=employeeID;
     this.FullName=fullName
     this.Department=department
     this.Level=level
-     this.Salary=salary
+    this.salaryAfterTax=0;
      allEmployee.push(this);
 }
 
-function Salary(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
- }
 
 
-let employee_1= new InformationsEmployee(1000,"Ghazi Samer","Administration", "Senior",Salary(1500,2000 ))
-let employee_2= new InformationsEmployee(1001,"Lana Ali","Finance","Senior",Salary(1500,2000) )
-let employee_3= new InformationsEmployee(1002,"Tamara Ayoub","Marketing","Senior",Salary(1500,2000) )
-let employee_4= new InformationsEmployee(1003,"Safi Walid","Administration", "Mid-Senior",Salary(1000,1500))
-let employee_5= new InformationsEmployee(1004,"Omar Zaid","Development","Senior", Salary(1500,2000) )
-let employee_6= new InformationsEmployee(1005,"Rana Saleh","Development","Junior",Salary(500,1000) )
-let employee_7= new InformationsEmployee(1006,"Hadi Ahmad","Finance","Mid-Senior", Salary(1000,1500) )
 
+let employee_1= new InformationsEmployee(1000,"Ghazi Samer",Department[0], "Senior")
+let employee_2= new InformationsEmployee(1001,"Lana Ali",Department[3],"Senior" )
+let employee_3= new InformationsEmployee(1002,"Tamara Ayoub",Department[1],"Senior" )
+let employee_4= new InformationsEmployee(1003,"Safi Walid",Department[0], "Mid-Senior")
+let employee_5= new InformationsEmployee(1004,"Omar Zaid",Department[2],"Senior" )
+let employee_6= new InformationsEmployee(1005,"Rana Saleh",Department[2],"Junior" )
+let employee_7= new InformationsEmployee(1006,"Hadi Ahmad",Department[3],"Mid-Senior" )
+
+
+InformationsEmployee.prototype.SalaryWithOutTax=function  (){
+  let Salary =0;
+  if(this.Level=="Senior"){
+   Salary= Math.random() * (2000 - 1500)  + 1500 ;
+   
+  }else if(this.Level=="Mid-Senior"){
+    Salary=Math.random() * (1500 - 1000)  + 1000;
+   
+  }else if(this.Level=="Junior"){
+    Salary= Math.random() * (1000 - 500)  + 500;
+    
+  }
+  
+  let taxFromSalary=(Salary*0.075);
+ 
+return   this.salaryAfterTax = Salary-taxFromSalary; 
+}
 
 
 InformationsEmployee.prototype.render=function(){
+let salaryAfterTax=this.SalaryWithOutTax();
      document.write(` <table calss="table"> 
      <tr>
      <th>Employee ID</th>
@@ -39,7 +59,7 @@ InformationsEmployee.prototype.render=function(){
      <td>${this.FullName}</td>
      <td>${this.Department}</td>
      <td>${this.Level}</td>
-     <td>${this.Salary}</td>
+     <td>${ this.salaryAfterTax}</td>
    </tr> </table>  `)
 }
 
@@ -63,13 +83,6 @@ console.table(allEmployee);
  
 
 
-// console.table(employee_1);
-// console.table(employee_2);
-// console.table(employee_3);
-// console.table(employee_4);
-// console.table(employee_5);
-// console.table(employee_6);
-// console.table(employee_7);
 
 
 
